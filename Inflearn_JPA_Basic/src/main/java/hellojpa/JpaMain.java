ce4665a35e076e4c50ec8ec98efdda3843253608
Yjunha <junay00v@gmail.com>
2024-07-22 16:30:30 +0900

    Section 3. 영속성 관리 - 내부 동작 방식 영속성 컨테스트 2

--- Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -15,19 +15,33 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            //비영속
+        /*
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
-            //영속
-            System.out.println("=== BEFORE ===");
             em.persist(member);
-            System.out.println("=== AFTER ===");
+            Member findMember = em.find(Member.class, 101);
+        */ //이런 경우 1차 캐시에서 조회
 
-//          em.detach(member); 회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
-//          em.remove(member); //객체를 삭제한 상태(삭제)
+//            Member findMember1 = em.find(Member.class, 101); // 다바에서 가져온 값을 영속성에 올려둠
+//            Member findMember2 = em.find(Member.class, 101); // 영속성 안에 있는 1차 캐시에서 조회
+//            System.out.println("result = " + (findMember1 == findMember2)); //true 동일성 보장
+
+            /*
+            //영속 //persist 할때 쿼리가 날라가는게 아니고 commit 할때 한꺼번에 쿼리를 날린다.
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("================");
+            */
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
+            //em.persist(member); //변경 감지로 인해서 필요 X
 
             tx.commit();
         } catch (Exception e) {
