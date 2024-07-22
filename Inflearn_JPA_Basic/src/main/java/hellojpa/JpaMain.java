443f688c5984226cf7999ab2ca6ee87b32993f9a
Yjunha <junay00v@gmail.com>
2024-07-22 16:37:19 +0900

    Section 3. 영속성 관리 - 내부 동작 방식 플러시

--- Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -15,34 +15,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-        /*
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
+            //영속
+            Member member = new Member(200L, "member200");
             em.persist(member);
-            Member findMember = em.find(Member.class, 101);
-        */ //이런 경우 1차 캐시에서 조회
 
-//            Member findMember1 = em.find(Member.class, 101); // 다바에서 가져온 값을 영속성에 올려둠
-//            Member findMember2 = em.find(Member.class, 101); // 영속성 안에 있는 1차 캐시에서 조회
-//            System.out.println("result = " + (findMember1 == findMember2)); //true 동일성 보장
+            em.flush(); // 호출 가능
 
-            /*
-            //영속 //persist 할때 쿼리가 날라가는게 아니고 commit 할때 한꺼번에 쿼리를 날린다.
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
-
-            System.out.println("================");
-            */
-
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZ");
-            //em.persist(member); //변경 감지로 인해서 필요 X
-
+            System.out.println("===============");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
