a3e4d87b198931f83315767b17f9fd192a9980b2
Yjunha <junay00v@gmail.com>
2024-07-22 19:07:39 +0900

    Section 4. 엔티티 매핑 기본 키 매핑

--- Inflearn_JPA_Basic/src/main/java/hellojpa/Member.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/Member.java
@@ -4,15 +4,10 @@ import jakarta.persistence.*;
 
 
 @Entity
-@TableGenerator(name = "MEMBER_SEQ_GENERATOR",
-        table = "MY_SEQUENCES",
-        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
-//@SequenceGenerator(name = "member_sqe_generator",sequenceName = "member_seq") 시퀀스 전략
 public class Member {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "member_sqe_generator")
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", nullable = false)
