0a5e6d311b113767e73ccfdd3c1949b5b8714348
Yjunha <junay00v@gmail.com>
2024-07-23 18:01:06 +0900

    Section 5. 연관관계 매핑 기초 단방향 연관관계

--- Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -13,20 +13,30 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            // 저장
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
+
+            em.flush();
+            em.clear();
 
             Member findMember = em.find(Member.class, member.getId());
 
-            Long findTeamId = findMember.getTeamId();
-            Team findTeam = em.find(Team.class,findTeamId);
-            
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
+
+            // 수정
+            Team newTeam = em.find(Team.class, 100L);
+            findMember.setTeam(newTeam);
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- Inflearn_JPA_Basic/src/main/java/hellojpa/Member.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/Member.java
@@ -6,15 +6,20 @@ import jakarta.persistence.*;
 @Entity
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
 
     @Column(name = "USERNAME")
     private String name;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -32,11 +37,11 @@ public class Member {
         this.name = name;
     }
 
-    public Long getTeamId() {
-        return teamId;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
