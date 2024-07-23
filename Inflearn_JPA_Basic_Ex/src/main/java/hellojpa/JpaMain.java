03626052ff26e38491058dc2612746df01a08a51
Yjunha <junay00v@gmail.com>
2024-07-23 17:12:13 +0900

    Section 4. 엔티티 매핑 실전 예제 1 - 요구사항 분석과 기본 매핑

--- Inflearn_JPA_Basic_Ex/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic_Ex/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import hellojpa.domain.Member;
+import hellojpa.domain.Order;
 import jakarta.persistence.*;
 
 public class JpaMain {
@@ -8,9 +10,24 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Order order = em.find(Order.class, 1L);
+//            Long memberId = order.getMemberId();
+//            Member member = em.find(Member.class,memberId); 관계형 디비에 맞춰서 설계
+
+            Member member = order.getMember(); // 객체에 맞춰서 설계
+            tx.commit();
+            ;
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
