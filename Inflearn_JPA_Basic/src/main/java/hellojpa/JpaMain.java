b1313462802a8fe8bba7a8fdf34ed79173888835
Yjunha <junay00v@gmail.com>
2024-07-24 14:20:42 +0900

    Section 8. 프록시와 연관관계 관리 프록시

--- Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -18,17 +18,17 @@ public class JpaMain {
             member1.setName("member1");
             em.persist(member1);
 
-            Member member2 = new Member();
-            member2.setName("member2");
-            em.persist(member2);
-
             em.flush();
             em.clear();
 
             Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.getReference(Member.class, member2.getId());
+            System.out.println("m1 = " + m1.getClass());
 
-            logic(m1, m2);
+            Member reference = em.getReference(Member.class, member1.getId());
+            System.out.println("reference = " + reference.getClass());
+
+            System.out.println("a == a : " + (m1 == reference));
+
 
             tx.commit();
         } catch (Exception e) {
@@ -38,10 +38,4 @@ public class JpaMain {
         }
         emf.close();
     }
-
-    private static void logic(Member m1, Member m2) {
-        System.out.println("m1 == m2 : " + (m1 instanceof Member));
-        System.out.println("m1 == m2 : " + (m2 instanceof Member));
-    }
-
 }
