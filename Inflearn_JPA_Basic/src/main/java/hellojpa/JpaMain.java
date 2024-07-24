41fe83c9ba3dca92b335b5f22071fbef8c83a1d1
Yjunha <junay00v@gmail.com>
2024-07-24 19:58:06 +0900

    Section 10. 객체지향 쿼리 언어1 - 기본 문법

--- Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -17,37 +17,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+            List<Member> result = em.createQuery(
+                    "select m from Member m where m.name like '%kim%'",Member.class
+            ).getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            //조회
-            System.out.println("========== Start ===========");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity -> newCity
-//            findMember.getHomeAddress().setCity("newCity"); 사이드 이펙트 발생우려 불변 객체 필요
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-//
-//            //치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-//
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+            for (Member member : result) {
+                System.out.println(member);
+            }
 
             tx.commit();
         } catch (Exception e) {
