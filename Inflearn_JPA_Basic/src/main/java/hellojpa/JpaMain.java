583c7d849602a2d3dfdba422d60c3425ce21608e
Yjunha <junay00v@gmail.com>
2024-07-24 18:30:26 +0900

    Section 9. 값 타입 값 타입과 불변 객체

--- Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -16,12 +16,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("hello");
-            member.setHomeAddress(new Address("city","street","zipcode"));
-            member.setWordPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setName("member1");
+            member1.setHomeAddress(address);
+            em.persist(member1);
+
+            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
+            member1.setHomeAddress(newAddress);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
