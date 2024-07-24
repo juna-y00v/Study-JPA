4fe1229ee60e955dd5e00ad234636bf965c1b390
Yjunha <junay00v@gmail.com>
2024-07-24 16:25:38 +0900

    Section 9. 값 타입 기본값 타입

---
+++ Inflearn_JPA_Basic_Ex/src/main/java/hellojpa/domain/ValueMain.java
@@ -0,0 +1,15 @@
+package hellojpa.domain;
+
+public class ValueMain {
+
+    public static void main(String[] args) {
+
+        Integer a = new Integer(10);
+        Integer b = a;
+
+        a = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
