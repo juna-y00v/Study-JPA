ae8bf2bb943332ab36985011d971a2bf18cb60d8
Yjunha <junay00v@gmail.com>
2024-07-25 20:34:04 +0900

    Section 11. 객체지향 쿼리 언어2 - 중급 문법 페치 조인1 - 기본

--- Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/JpaMain.java
@@ -20,28 +20,40 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username From Team t join t.members m";//명시적 조인후 별칭 가져오기
+            String query = "select m From Member m join fetch m.team";
 
-            List<Collection> result = em.createQuery(query, Collection.class)
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
-            System.out.println("result = " + result);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() +", " + member.getTeam().getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
