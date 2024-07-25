8d80b552fb60b79791b53ea81c898248f6d3ce96
Yjunha <junay00v@gmail.com>
2024-07-25 19:25:01 +0900

    Section 10. 객체지향 쿼리 언어1 - 기본 문법 조건식(CASE 등등)

--- Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/JpaMain.java
@@ -33,17 +33,18 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', true from Member m " +
-                    "where m.type = :userType";
+            String query =
+                    "select " +
+                            "case when m.age <= 10 then '학생요금'" +
+                            "     when m.age >= 60 then '경로요금'" +
+                            "     else '일반요금' " +
+                            "end " +
+                            "from Member m";
 
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType",MemberType.ADMIN)
-                            .getResultList();
-
-            for (Object[] objects : result) {
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
+            List<String> result = em.createQuery(query, String.class)
+                    .getResultList();
+            for (String string : result) {
+                System.out.println("string = " + string);
             }
 
 
