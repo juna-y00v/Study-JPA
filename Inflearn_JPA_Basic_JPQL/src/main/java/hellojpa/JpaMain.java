90781cef1e583f30861209447a608b75ea544b1f
Yjunha <junay00v@gmail.com>
2024-07-25 17:15:27 +0900

    Section 10. 객체지향 쿼리 언어1 - 기본 문법 기본 문법과 쿼리 API

--- Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import hellojpa.jpql.Member;
 import jakarta.persistence.*;
 
 public class JpaMain {
@@ -8,9 +9,33 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query1= em.createQuery("select m from Member m", Member.class);
+            //타입 정보 명확
+            TypedQuery<String> query2= em.createQuery("select m.username from Member m", String.class);
+            //타입 정보 명확 TypedQuery 사용가능
+            Query query3= em.createQuery("select m.username, m.age from Member m");
+            //타입 정복 불명확하기에 Query 사용
+            
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
