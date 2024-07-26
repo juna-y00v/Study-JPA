2c24f3e73e0de6459d58ff8de875906b3b75cbc2
Yjunha <junay00v@gmail.com>
2024-07-26 22:11:37 +0900

    Section 6. 주문 도메인 개발 주문 서비스 개발

--- Inflearn_JPA_Usage1/src/main/java/jpabook/jpashop/domain/Order.java
+++ Inflearn_JPA_Usage1/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.aspectj.weaver.ast.Or;
 
@@ -15,6 +17,7 @@ import static jakarta.persistence.FetchType.*;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
