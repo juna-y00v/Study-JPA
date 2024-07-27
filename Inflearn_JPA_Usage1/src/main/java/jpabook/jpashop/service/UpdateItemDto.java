515d4358267e7782aa9451326239d9a4f7ab08e1
junay00v <junay00v@gmail.com>
2024-07-27 17:37:07 +0900

    Section 7. 웹 계층 개발 변경 감지와 병합(merge)

---
+++ Inflearn_JPA_Usage1/src/main/java/jpabook/jpashop/service/UpdateItemDto.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.service;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateItemDto {//Dto 사용해서 더 유지보수 쉽게 코드 작성 가능 ex) update 메서드 쪽
+}
