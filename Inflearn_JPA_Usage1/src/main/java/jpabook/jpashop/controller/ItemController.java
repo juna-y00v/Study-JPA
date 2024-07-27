b72595af361c26bb2f5de65b492a4e29e8248bc0
junay00v <junay00v@gmail.com>
2024-07-27 16:30:01 +0900

    Section 7. 웹 계층 개발 상품 목록

--- Inflearn_JPA_Usage1/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ Inflearn_JPA_Usage1/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.controller;
 
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,5 +36,12 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
