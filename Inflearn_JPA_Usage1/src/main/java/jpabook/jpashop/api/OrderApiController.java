db1f785e48ab9795ae85795372abbb4c47e066c4
junay00v <junay00v@gmail.com>
2024-07-30 14:03:46 +0900

    Section 4.(활용2) API 개발 고급 - 컬렉션 조회 최적화 주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- Inflearn_JPA_Usage1/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ Inflearn_JPA_Usage1/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -80,6 +80,11 @@ public class OrderApiController {
        return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return orderQueryRepository.finAllByDto_optimization();
+    }
+
     @Data
     static class OrderDto {
 
