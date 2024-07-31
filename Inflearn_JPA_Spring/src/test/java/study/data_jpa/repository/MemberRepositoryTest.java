512c4814d972a557087ba44e4b76680ec239de31
junay00v <junay00v@gmail.com>
2024-07-31 18:01:56 +0900

    Section 4. 쿼리 메서드 기능 @Query, 리포지토리 메서드에 쿼리 정의하기

--- Inflearn_JPA_Spring/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ Inflearn_JPA_Spring/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import study.data_jpa.entity.Member;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -89,5 +90,16 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findByUsername("AAA");
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(m1);
+    }
+
+    @Test
+    public void testQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.get(0)).isEqualTo(m1);
     }
 }
