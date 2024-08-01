faaebf224d1ef59ea07539aa865114a6a86dad57
junay00v <junay00v@gmail.com>
2024-08-01 14:26:00 +0900

    Section 5. 확장 기능 Auditing

--- Inflearn_JPA_Spring/src/main/java/study/data_jpa/DataJpaApplication.java
+++ Inflearn_JPA_Spring/src/main/java/study/data_jpa/DataJpaApplication.java
@@ -2,12 +2,25 @@ package study.data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
 
+    @Bean
+    public AuditorAware<String> auditorProvier() {
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
