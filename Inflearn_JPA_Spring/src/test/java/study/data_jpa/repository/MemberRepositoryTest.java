3018c5cfc0124cd1dcb13daf2300f4b15a77679c
junay00v <junay00v@gmail.com>
2024-08-01 16:54:46 +0900

    Section 7. 나머지 기능들 Projections

--- Inflearn_JPA_Spring/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ Inflearn_JPA_Spring/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -330,12 +330,36 @@ class MemberRepositoryTest {
         Team team = new Team("teamA");
         member.setTeam(team);
 
-        ExampleMatcher matcher = ExampleMatcher.matching()
-                .withIgnorePaths("age");
-        Example<Member> example = Example.of(member,matcher);
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("age");
+        Example<Member> example = Example.of(member, matcher);
 
         List<Member> result = memberRepository.findAll(example);
 
         assertThat(result.get(0).getUsername()).isEqualTo("m1");
+    }
+
+    @Test
+    public void projections() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        List<NestedClosedProjections> result = memberRepository.findProjectionsByUsername("m1", NestedClosedProjections.class);
+
+        for (NestedClosedProjections nestedClosedProjections : result) {
+            String username = nestedClosedProjections.getUsername();
+            System.out.println("username = " + username);
+            String teamName = nestedClosedProjections.getTeam().getName();
+            System.out.println("teamName = " + teamName);
+        }
     }
 }
