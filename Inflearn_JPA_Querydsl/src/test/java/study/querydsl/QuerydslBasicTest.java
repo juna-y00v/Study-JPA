b76be1d387454f34716a5f4f17ba68b0ccffbdbf
junay00v <junay00v@gmail.com>
2024-08-02 18:36:42 +0900

    Section 4. 중급 문법 수정, 삭제 벌크 연산

--- Inflearn_JPA_Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ Inflearn_JPA_Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
 import study.querydsl.dto.QMemberDto;
@@ -645,5 +646,51 @@ public class QuerydslBasicTest {
 
     private BooleanExpression allEq(String usernameCond, Integer ageCond) {
         return usernameEq(usernameCond).and((ageEq(ageCond)));
+    }
+
+    @Test
+    public void bulkUpdate() {
+        em.flush();
+        em.clear();
+
+        //member1 = 10 -> 1 DB member1
+        //member2 = 20 -> 2 DB member2
+        //member3 = 30 -> 3 DB member3
+        //member4 = 40 -> 4 DB member4
+
+        long count = query
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28))
+                .execute();
+
+        //member1 = 10 -> 1 DB 비회원
+        //member2 = 20 -> 2 DB 비회원
+        //member3 = 30 -> 3 DB member3
+        //member4 = 40 -> 4 DB member4
+
+        List<Member> result = query
+                .selectFrom(member)
+                .fetch();
+
+        for (Member member1 : result) {
+            System.out.println("member1 = " + member1);
+        }
+    }
+
+    @Test
+    public void bulkAdd() {
+        long count = query
+                .update(member)
+                .set(member.age, member.age.add(1))
+                .execute();
+    }
+
+    @Test
+    public void bulkDelete() {
+        long count = query
+                .delete(member)
+                .where(member.age.gt(18))
+                .execute();
     }
 }
