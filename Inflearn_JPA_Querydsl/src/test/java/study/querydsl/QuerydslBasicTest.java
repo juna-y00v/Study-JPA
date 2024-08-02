d524b2163abbc295ace452cc47d14687afc05d9d
junay00v <junay00v@gmail.com>
2024-08-02 18:56:17 +0900

    Section 4. 중급 문법 SQL function 호출 하기

--- Inflearn_JPA_Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ Inflearn_JPA_Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -693,4 +693,34 @@ public class QuerydslBasicTest {
                 .where(member.age.gt(18))
                 .execute();
     }
+
+    @Test
+    public void sqlFunction() {
+        List<String> result = query
+                .select(
+                        Expressions.stringTemplate(
+                                "function('replace', {0}, {1}, {2})",
+                                member.username, "member1", "M"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void sqlFunction2() {
+        List<String> result = query
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower',{0})", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
