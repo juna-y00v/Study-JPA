847a646489e98e0910105d53cbd71ee5ab2b651f
junay00v <junay00v@gmail.com>
2024-08-02 17:02:43 +0900

    Section 3. 기본 문법 상수, 문자 더하기

--- Inflearn_JPA_Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ Inflearn_JPA_Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -4,6 +4,7 @@ package study.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -441,6 +442,32 @@ public class QuerydslBasicTest {
                         .when(member.age.between(21, 39)).then("21~30살")
                         .otherwise("기타"))
                 .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void constant() {
+        List<Tuple> result = query
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat() {
+        //{username}_{age}
+        List<String> result = query
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetch();
 
         for (String s : result) {
